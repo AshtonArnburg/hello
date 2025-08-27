81c1053e63081c267fddd0237e529a9b1a709197
Ashton Arnburg <aarnburg2@huskers.unl.edu>
2025-08-26 21:06:16 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Ashton Arnburg
+ *         aarnburg2@huskers.unl.edu
+ * Date: 2025/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Ashton Arnburg, Computer Engineering\n");
+  printf("I love the School of Computing.\n");
+
+  return 0;
+}
